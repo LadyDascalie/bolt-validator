6825293368726196b7b7c012d32dca0a94c8e54a
Benjamin Cable <cable.benjamin@gmail.com>
2016-04-08 12:51:26 +0100

    Corrected strtolower call within method
    Signed-off-by: Benjamin Cable <cable.benjamin@gmail.com>

--- src/Validator.php
+++ src/Validator.php
@@ -23,8 +23,7 @@
 		 */
 		public function uuid($UUID)
 		{
-            strtolower($UUID);
-            if (preg_match('/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/', $UUID)) {
+            if (preg_match('/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/', strtolower($UUID))) {
 				if ($this->throw === true)
 				{
 					throw new Exceptions\Validation("Invalid UUID provided");
